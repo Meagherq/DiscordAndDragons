3914ab574fa54b8fe58a3a5309b988f0960c4a34
Mason Kochis <162236253+MkochisCode@users.noreply.github.com>
2024-03-09 19:39:17 -0500

    Move Player Commands to Enum
    Player Commands have been moved to an enum for more rigidity

---
+++ Grains/Enums/PlayerCommands.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adventure.Abstractions;
+
+namespace Adventure.Grains.Enums;
+enum PlayerCommands
+{
+    look,
+    go,
+    north,
+    south,
+    east,
+    west,
+    kill,
+    drop,
+    take,
+    inv,
+    inventory,
+    measure,
+    end
+}
---
+++ Grains/Extensions/EnumExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adventure.Grains.Enums;
+
+namespace Adventure.Grains.Extensions;
+public static class EnumExtensions
+{
+    public static string ToDescriptionString(this Enum val)
+    {
+        var attributes = (DescriptionAttribute[])val
+           .GetType()
+           .GetField(val.ToString())
+           .GetCustomAttributes(typeof(DescriptionAttribute), false);
+        return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+    }
+}
