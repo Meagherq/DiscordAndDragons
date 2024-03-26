8bc30a3d3ba768e83b4c363565d08d8929889741
Mason Kochis <162236253+MkochisCode@users.noreply.github.com>
2024-03-26 07:48:40 -0400

    Begin work on Web Interfaces

---
+++ Silo/Pages/AdventureInterface.razor.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Adventure.Silo.Pages;
+
+public sealed partial class AdventureInterface
+{
+}
---
+++ Silo/Pages/Index.razor.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Text.RegularExpressions;
+
+namespace Adventure.Silo.Pages;
+
+public sealed partial class Index
+{
+    private int? AdventureId;
+    private string _adventureName;
+    private bool createAdventureEnabled = true;
+    private string? _value;
+    private string? _result;
+    private List<AdventureInfo> _adventures = new();
+    string AdventureName
+    {
+        get { return _adventureName; }
+        set
+        {
+            _adventureName = Regex.Replace(value, @"[^a-zA-Z]", "");
+        }
+    }
+    [Inject]
+    public AdventureService _adventureService { get; set; } = null!;
+    [Inject]
+    public AdventureLogService _adventureLogService { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _adventures = await _adventureLogService.ListAdventures();
+        base.OnInitializedAsync();
+    }
+    private async Task CreateSession()
+    {
+        AdventureId = await _adventureService.Create(AdventureName);
+        await ProtectedSessionStore.SetAsync("AdventureId", AdventureId);
+    }
+
+    private void UpdateAdventureId(string? e)
+    {
+        AdventureId = int.TryParse(e.ToString(), out var result) ? result : null;
+    }
+    private async void UpdateAdventureName(string? e)
+    {
+        _adventureName = e;
+        _value = e;
+        if (e.Length > 0)
+        {
+            createAdventureEnabled = false;
+        }
+        else
+        {
+            createAdventureEnabled = true;
+        }
+    }
+}
